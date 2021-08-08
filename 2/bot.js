abd037f17fda73759ef2b183bcb9e3c43d3303fc
Ivan Labiak <labyak.ivan2003@gmail.com>
2021-08-08 14:34:36 +0300

    fixing bot settings

--- 2/bot.js
+++ 2/bot.js
@@ -1,21 +1,13 @@
 const { Telegraf } = require('telegraf')
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
-const fetch = require('node-fetch');
 
-const bot = new Telegraf('234')
+const bot = new Telegraf('')
     bot.start((ctx) => ctx.reply('Welcome!'))
     bot.help((ctx) => ctx.reply('Send me a sticker'))
     bot.on('sticker', (ctx) => ctx.reply('👍'))
     bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+    bot.use()
+    bot.launch()
 
 
-bot.command('cryptoprices', async (ctx) => {
-    let currency = ctx.update.message.text.split(' ')[1];
-    if(currency === undefined || currency.toLowerCase() =='usd'){
-        ctx.reply(await crypto.cryptoPrices('btc','eth','ltc'));
-        return;
-    }
-    ctx.reply(await crypto.cryptoPrices(currency));
-});
