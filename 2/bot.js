53f5a0176d513f6c2638376152c0b98b8b9e4fa0
Ivan Labiak <labyak.ivan2003@gmail.com>
2021-08-11 12:46:58 +0300

    Prettier

--- 2/bot.js
+++ 2/bot.js
@@ -1,104 +1,105 @@
-'use strict';
+"use strict";
 
-const { Telegraf} = require('telegraf')
-const fetch = require('node-fetch');
-const fs = require('fs');
+const { Telegraf } = require("telegraf");
+const fetch = require("node-fetch");
+const fs = require("fs");
 
-
-  const bot = new Telegraf('')
-  bot.start((ctx) => ctx.reply('Привет, это чекер куков Instagram!'))
-  bot.use()
-  bot.launch()
-  bot.on('document', async(ctx) => {
-    let fileId = ctx.update.message.document.file_id;
-   // console.log(fileId);
-    let link = await ctx.telegram.getFileLink(fileId);
-   // console.log(link)
-    await downloadFile(link);
-    let cookies = await getSessionIds();
-    let results = []
-    for(let cookie of cookies){
-      results.push(await checkForValid(cookie));
-    }
-    await ctx.reply(results.join('\n'));
-    cookies = [];
-
-  })
-
-  const downloadFile = (async (url, path = './cookies/1.txt') => {
-    const res = await fetch(url);
-    const fileStream = fs.createWriteStream(path);
-    await new Promise((resolve, reject) => {
-        res.body.pipe(fileStream);
-        res.body.on("error", reject);
-        fileStream.on("finish", resolve);
-      });
-  });
-
-  const getSessionIds = (async (path ='cookies/1.txt') => {
-    let textArr = fs.readFileSync(path, 'utf8').split('\n');
-    let sessionIds = [];
-       for(let el of textArr){
-       if(el.includes('.instagram.com') && el.includes('sessionid')){
-    let start = el.lastIndexOf('\t')+1;
-    let end = el.lastIndexOf('\r');
-    let sessionId = el.slice(start,end);
-    sessionIds.push(sessionId);
-        }
-     }
-     return sessionIds;
-  });
-
-  const checkForValid = (async(sessionIdCookie) => {
-    if(typeof(sessionIdCookie)=== 'string'){
-        let request = await fetch('https://www.instagram.com/',{
-            headers: {'Cookie': `sessionid=${sessionIdCookie}; Domain=.instagram.com; `}
-        })
-        .then(request => request.text())
-        .then(async body => {
-            let data = [];
-            let arr = body.split('\n');
-            for(let el of arr){
-                if(el.includes('viewer":{"biography')){
-                    data.push(el);
-                }
-            }
-            if(data[0]){
-                let text = data[0];
-                let usernameStart = text.indexOf('username":"')+11;
-                let usernameEnd = text.indexOf('","badge_count"');
-                let username = text.slice(usernameStart,usernameEnd);
-                let usernameOutput = `https://www.instagram.com/${username}/`;
-                
-                let secondRequest = await fetch(usernameOutput,{
-                    headers: {'Cookie': `sessionid=${sessionIdCookie}; Domain=.instagram.com; `}
-                })
-                .then(secondRequest =>  secondRequest.text())
-                .then(body => {
-                    let data = [];
-                    let arr = body.split('\n');
-                        for(let el of arr){
-                     if(el.includes('<meta content="')){
-                    data.push(el);
-                }}
-                if(data[0]){
-                    let text = data[0]
-                    let startStr = text.indexOf('content="')+9
-                    let endStr = text.indexOf('- See Instagram photos')
-                    let accountInfo = text.slice(startStr,endStr)
-                    return `${usernameOutput}\n${accountInfo}`;
-
-                    }
-                })
-            return secondRequest;
-            }else{
-                return 'Невалид';
-            }
-        })
-        return request; 
-    }else{
-        ctx.reply('wrong format');
-        ctx.reply(typeof(sessionIdCookie));
-    }
+const bot = new Telegraf("");
+bot.start((ctx) => ctx.reply("Привет, это чекер куков Instagram!"));
+bot.use();
+bot.launch();
+bot.on("document", async (ctx) => {
+  let fileId = ctx.update.message.document.file_id;
+  // console.log(fileId);
+  let link = await ctx.telegram.getFileLink(fileId);
+  // console.log(link)
+  await downloadFile(link);
+  let cookies = await getSessionIds();
+  let results = [];
+  for (let cookie of cookies) {
+    results.push(await checkForValid(cookie));
+  }
+  await ctx.reply(results.join("\n"));
+  cookies = [];
 });
 
+const downloadFile = async (url, path = "./cookies/1.txt") => {
+  const res = await fetch(url);
+  const fileStream = fs.createWriteStream(path);
+  await new Promise((resolve, reject) => {
+    res.body.pipe(fileStream);
+    res.body.on("error", reject);
+    fileStream.on("finish", resolve);
+  });
+};
+
+const getSessionIds = async (path = "cookies/1.txt") => {
+  let textArr = fs.readFileSync(path, "utf8").split("\n");
+  let sessionIds = [];
+  for (let el of textArr) {
+    if (el.includes(".instagram.com") && el.includes("sessionid")) {
+      let start = el.lastIndexOf("\t") + 1;
+      let end = el.lastIndexOf("\r");
+      let sessionId = el.slice(start, end);
+      sessionIds.push(sessionId);
+    }
+  }
+  return sessionIds;
+};
+
+const checkForValid = async (sessionIdCookie) => {
+  if (typeof sessionIdCookie === "string") {
+    let request = await fetch("https://www.instagram.com/", {
+      headers: {
+        Cookie: `sessionid=${sessionIdCookie}; Domain=.instagram.com; `,
+      },
+    })
+      .then((request) => request.text())
+      .then(async (body) => {
+        let data = [];
+        let arr = body.split("\n");
+        for (let el of arr) {
+          if (el.includes('viewer":{"biography')) {
+            data.push(el);
+          }
+        }
+        if (data[0]) {
+          let text = data[0];
+          let usernameStart = text.indexOf('username":"') + 11;
+          let usernameEnd = text.indexOf('","badge_count"');
+          let username = text.slice(usernameStart, usernameEnd);
+          let usernameOutput = `https://www.instagram.com/${username}/`;
+
+          let secondRequest = await fetch(usernameOutput, {
+            headers: {
+              Cookie: `sessionid=${sessionIdCookie}; Domain=.instagram.com; `,
+            },
+          })
+            .then((secondRequest) => secondRequest.text())
+            .then((body) => {
+              let data = [];
+              let arr = body.split("\n");
+              for (let el of arr) {
+                if (el.includes('<meta content="')) {
+                  data.push(el);
+                }
+              }
+              if (data[0]) {
+                let text = data[0];
+                let startStr = text.indexOf('content="') + 9;
+                let endStr = text.indexOf("- See Instagram photos");
+                let accountInfo = text.slice(startStr, endStr);
+                return `${usernameOutput}\n${accountInfo}`;
+              }
+            });
+          return secondRequest;
+        } else {
+          return "Невалид";
+        }
+      });
+    return request;
+  } else {
+    ctx.reply("wrong format");
+    ctx.reply(typeof sessionIdCookie);
+  }
+};
--- 2/test.js
+++ 2/test.js
@@ -1,63 +1,65 @@
-'use strict';
+"use strict";
 
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-const checkForValid = (async(sessionIdCookie) => {
-    if(typeof(sessionIdCookie)=== 'string'){
-        let request = await fetch('https://www.instagram.com/',{
-            headers: {'Cookie': `sessionid=${sessionIdCookie}; Domain=.instagram.com; `}
-        })
-        .then(request => request.text())
-        .then(async body => {
-            let data = [];
-            let arr = body.split('\n');
-            for(let el of arr){
-                if(el.includes('viewer":{"biography')){
-                    data.push(el);
+const checkForValid = async (sessionIdCookie) => {
+  if (typeof sessionIdCookie === "string") {
+    let request = await fetch("https://www.instagram.com/", {
+      headers: {
+        Cookie: `sessionid=${sessionIdCookie}; Domain=.instagram.com; `,
+      },
+    })
+      .then((request) => request.text())
+      .then(async (body) => {
+        let data = [];
+        let arr = body.split("\n");
+        for (let el of arr) {
+          if (el.includes('viewer":{"biography')) {
+            data.push(el);
+          }
+        }
+        if (data[0]) {
+          let text = data[0];
+          let usernameStart = text.indexOf('username":"') + 11;
+          let usernameEnd = text.indexOf('","badge_count"');
+          let username = text.slice(usernameStart, usernameEnd);
+          let usernameOutput = `https://www.instagram.com/${username}/`;
+
+          let secondRequest = await fetch(usernameOutput, {
+            headers: {
+              Cookie: `sessionid=${sessionIdCookie}; Domain=.instagram.com; `,
+            },
+          })
+            .then((secondRequest) => secondRequest.text())
+            .then((body) => {
+              let data = [];
+              let arr = body.split("\n");
+              for (let el of arr) {
+                if (el.includes('<meta content="')) {
+                  data.push(el);
                 }
-            }
-            if(data[0]){
+              }
+              if (data[0]) {
                 let text = data[0];
-                let usernameStart = text.indexOf('username":"')+11;
-                let usernameEnd = text.indexOf('","badge_count"');
-                let username = text.slice(usernameStart,usernameEnd);
-                let usernameOutput = `https://www.instagram.com/${username}/`;
-                
-                let secondRequest = await fetch(usernameOutput,{
-                    headers: {'Cookie': `sessionid=${sessionIdCookie}; Domain=.instagram.com; `}
-                })
-                .then(secondRequest =>  secondRequest.text())
-                .then(body => {
-                    let data = [];
-                    let arr = body.split('\n');
-                        for(let el of arr){
-                     if(el.includes('<meta content="')){
-                    data.push(el);
-                }}
-                if(data[0]){
-                    let text = data[0]
-                    let startStr = text.indexOf('content="')+9
-                    let endStr = text.indexOf('- See Instagram photos')
-                    let accountInfo = text.slice(startStr,endStr)
-                    return `${usernameOutput}\n${accountInfo}`;
+                let startStr = text.indexOf('content="') + 9;
+                let endStr = text.indexOf("- See Instagram photos");
+                let accountInfo = text.slice(startStr, endStr);
+                return `${usernameOutput}\n${accountInfo}`;
+              }
+            });
+          return secondRequest;
+        } else {
+          return "Невалид";
+        }
+      });
+    return request;
+  } else {
+    ctx.reply("wrong format");
+    ctx.reply(typeof sessionIdCookie);
+  }
+};
 
-                    }
-                })
-            return secondRequest;
-            }else{
-                return 'Невалид';
-            }
-        })
-        return request; 
-    }else{
-        ctx.reply('wrong format');
-        ctx.reply(typeof(sessionIdCookie));
-    }
-});
-
-(async()=>{
-
- console.log(await checkForValid('198404570%3AzaNKYcYR1xgwAY%3A19'))
-//console.log(res)
-
-})()
+(async () => {
+  console.log(await checkForValid("198404570%3AzaNKYcYR1xgwAY%3A19"));
+  //console.log(res)
+})();
