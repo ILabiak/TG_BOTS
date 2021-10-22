44cd75f680998fbe7bbeffe4e8f9ca16bf8281e7
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2021-10-22 13:41:10 +0300

    testing web vscode

--- 2/test2.js
+++ 2/test2.js
@@ -3,4 +3,4 @@
 let str = '.instagram.com\tTRUE\t/\tFALSE\t1830365600\tsessionid\t21649066289%3AQomTHwVqavEHNR%3A0'
 
 console.log(str.lastIndexOf("\t"))
-console.log(str.slice())
+console.log(str.slice());
