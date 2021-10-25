096528755efde3104ed05e19deeec390c2206c88
Ivan Labiak <labyak.ivan2003@gmail.com>
2021-10-25 20:17:20 +0300

    made a scene to request payment amount

--- 4/bot.js
+++ 4/bot.js
@@ -1,10 +1,14 @@
 "use strict";
 
-//const config = require('config');
 const config = require('./config/config.json');
+const Telegraf = require('telegraf')
+const session = require('telegraf/session')
+const Stage = require('telegraf/stage')
+const Scene = require('telegraf/scenes/base')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+const { enter, leave } = Stage
 
-const { Telegraf, Extra } = require("telegraf");
-const { bot_token } = require('./config/default');
 const QiwiApi = require('./qiwi')
 const bot = new Telegraf(config.bot_token);
 
@@ -13,15 +17,47 @@ const qiwi = new QiwiApi({
   personId: config.qiwi_number // Номер кошелька
 });
 
-bot.start((ctx) => ctx.reply("Привет, это бот nakru-ti!!!"));
-bot.use();
+const paymentAmountScene = new Scene('paymentAmount')
+paymentAmountScene.enter(({ reply }) =>
+reply('Введите сумму пополнения(не менее 50 рублей):', Markup
+  .keyboard(['Меню'])
+  .oneTime()
+  .resize()
+  .extra()
+))
+paymentAmountScene.hears('Отменить', leave('greeter'))
+paymentAmountScene.on('message', (ctx) => {
+if(parseInt(ctx.message.text)>=50){
+  
+}
+else{
+  ctx.scene.enter('greeter')
+}
+})
+
+
+
+
+const stage = new Stage([paymentAmountScene, paymentMethodScene], { ttl: 10 })
+bot.use(session())
+bot.use(stage.middleware())
+bot.hears('Пополнить💲', (ctx) => ctx.scene.enter('paymentAmount'))
+bot.command('test', ({ reply }) =>
+  reply('Выберите действие', Markup
+    .keyboard(['Пополнить💲'])
+    .oneTime()
+    .resize()
+    .extra()
+  )
+)
 bot.launch();
+
+
 
 
 bot.on('sticker', async (ctx) =>{
   //console.log(ctx.update.message.message_id);
   let messageId = ctx.update.message.message_id;
-  ctx.reply('response', Extra.inReplyTo(messageId))
 })
 
 const receivePayment = async(paymentComment, sum) =>{
