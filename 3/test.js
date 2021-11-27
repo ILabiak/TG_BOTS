2658f697ccbb3d572813356514ea5167e5d33aa1
Ivan Labiak <labyak.ivan2003@gmail.com>
2021-11-27 17:03:24 +0200

    made function to get txt files from dir

--- 3/test.js
+++ 3/test.js
@@ -5,6 +5,7 @@ let { zip, unzip } = require('cross-unzip')
 
 const dirnames = fs.readdirSync('./3')
 const filename = './3/download/insta.zip'
+const dir = './3/download/insta'
 
 
 const extractArchieve = (filename) =>{
@@ -13,11 +14,29 @@ const extractArchieve = (filename) =>{
         fs.mkdirSync(newDir);
     }
     unzip(filename, newDir, err => {
-        console.log(err)
+
       })
-      return true;
+      return newDir;
 }
 
-
+const getTxtfiles = (dir) =>{
+    let txtFilesArr = [];
+    let filesArr = fs.readdirSync(dir);
+    for(let el of filesArr){
+if(el.slice(-3)== 'txt'){
+txtFilesArr.push(dir + '/' + el)
+let index = filesArr.indexOf(el)
+delete filesArr[index]
+}
+    }
+    filesArr = filesArr.filter(n => n)
+    if(filesArr[0]){
+        for(let el of filesArr){
+            txtFilesArr.push(getTxtfiles(dir+'/'+el))
+        }
+    }
+    return txtFilesArr.flat();
+}
 //console.log(filename.slice(0,-4))
-extractArchieve(filename)
+//extractArchieve(filename)
+console.dir(getTxtfiles(dir))
