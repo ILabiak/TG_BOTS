0f4597d8d24de6951aeb07b63c7ade4d88c330ee
Ivan Labiak <labyak.ivan2003@gmail.com>
2021-12-12 17:19:45 +0200

    f

--- 4/test.js
+++ 4/test.js
@@ -1,7 +1,4 @@
 'use strict'
 
 
-let link = 'instagram.com/123'
-let linkRegex = new RegExp(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/)
 
-console.dir(regex.test(link))
