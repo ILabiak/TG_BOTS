9c39fc120d40df1dad7b218ce2e6b15f0955a383
Ivan Labiak <labyak.ivan2003@gmail.com>
2022-02-24 19:34:57 +0200

    made bot to anonymously exchange messages

--- 5/bot.js
+++ 5/bot.js
@@ -10,35 +10,67 @@ const Markup = require("telegraf/markup");
 
 const bot = new Telegraf(config.bot_token);
 
-bot.use(session());
 /* bot.catch((err) => {
   console.log(err)
 }) */
 
-bot.hears("AA", (ctx) => ctx.reply("TEST"));
+const sendMessageScene = new Scene("sendMessage");
+sendMessageScene.enter((ctx) => {
+  ctx.reply(`Відправте анонімне повідомлення користувачу, який опублікував це посилання.
+
+Напиши сюди в одному повідомленні все, що ти хочеш сказати отримувачу і через декілька секунд він отримає його.`);
+});
+sendMessageScene.on("message", async (ctx) => {
+  const senderId = ctx.session.__scenes.state.senderId;
+  const tgId = ctx.session.__scenes.state.tgId;
+  if (tgId) {
+    await bot.telegram
+      .sendMessage(tgId, "Вам прийшло нове анонімне повідомлення:")
+      .catch((e) => {});
+    await bot.telegram
+      .sendMessage(tgId, ctx.update.message.text, {
+        reply_markup: {
+          inline_keyboard: [[{ text: "Відповісти", callback_data: `reply_${senderId}` }]],
+        },
+      })
+      .catch((e) => {
+        ctx.reply(
+          "Користувач, якому ви хочете написати, вже не користується цим ботом."
+        );
+      });
+    ctx.scene.leave("sendMessage");
+  }
+});
+
+const stage = new Stage([sendMessageScene]);
+
+bot.use(session());
+bot.use(stage.middleware());
+
+bot.launch();
 
 bot.command("start", async (ctx) => {
   const text = ctx.update.message.text;
   if (text.includes("/start ")) {
-    const startId = parseInt(text.replace("/start ", 0));
-    bot.telegram.sendMessage(startId, "test").catch((e) => {
-      ctx.reply(
-        "Користувач, якому ви хочете написати, вже не користується цим ботом."
-      );
-    });
+    const senderId = ctx.update.message.from.id;
+    const startId = parseInt(text.replace("/start ", ""));
+    ctx.scene.enter("sendMessage", { tgId: startId, senderId: senderId });
   } else {
     const userId = ctx.update.message.from.id;
     ctx.reply(`
-Привіт, це бот для анонімного обміну повідомленнями.
-
-Щоб отримати повідомлення від когось, розмісти це посилання в своїх соц. мережах:
-https://t.me/anonymus3000bot?start=${userId}
-
-`);
+  Привіт, це бот для анонімного обміну повідомленнями.
+  
+  Щоб отримати повідомлення від когось, розмісти це посилання в своїх соц. мережах:
+  https://t.me/anonymus3000bot?start=${userId}
+  `);
   }
 });
 
-bot.launch();
+bot.action(/reply_[0-9]*/, (ctx) => {
+    const senderId = ctx.update.callback_query.from.id;
+    const startId = parseInt(ctx.update.callback_query.data.replace('reply_', ''))
+    ctx.scene.enter("sendMessage", { tgId: startId, senderId: senderId });
+});
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
